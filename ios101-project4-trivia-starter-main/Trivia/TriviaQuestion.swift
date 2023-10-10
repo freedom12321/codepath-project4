26e98bc751d62bc3de34cc339f3f42e8d6b6399a
freedom12321 <52187436+freedom12321@users.noreply.github.com>
2023-10-10 01:49:15 -0500

    update to it

---
+++ ios101-project4-trivia-starter-main/Trivia/TriviaQuestion.swift
@@ -0,0 +1,16 @@
+//
+//  TriviaQuestion.swift
+//  Trivia
+//
+//  Created by Mari Batilando on 4/6/23.
+//
+
+import Foundation
+
+struct TriviaQuestion: Decodable {
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+}
+
